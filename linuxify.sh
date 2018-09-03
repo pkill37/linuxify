b3a07275c60089cac3046fec2660ba525a7ef511
Fábio Maia <fabiu.maia@gmail.com>
2018-09-03 17:21:25 +0100

    Fix uninstall and cleanup formulas list

--- linuxify.sh
+++ linuxify.sh
@@ -9,6 +9,10 @@ fi
 
 formulas=(
     # GNU programs non-existing in macOS
+    "watch"
+    "wget"
+    "wdiff --with-gettext"
+    "gdb"
 
     # GNU programs whose BSD counterpart is installed in macOS
     "coreutils"
@@ -21,25 +25,21 @@ formulas=(
     "gnu-sed --with-default-names"
     "gnu-tar --with-default-names"
     "gnu-which --with-default-names"
-    "gnutls"
     "grep --with-default-names"
     "gzip"
     "screen"
-    "watch"
-    "wdiff --with-gettext"
-    "wget"
 
     # GNU programs existing in macOS which are outdated
     "bash"
     "emacs"
-    "gdb"  # gdb requires further actions to make it work. See `brew info gdb`.
     "gpatch"
     "less"
     "m4"
     "make --with-default-names"
     "nano"
 
-    # Other common programs in GNU/Linux distributions
+    # Other common/preferred programs in GNU/Linux distributions
+    "libressl"
     "file-formula"
     "git"
     "openssh"
@@ -49,12 +49,13 @@ formulas=(
     "svn"
     "unzip"
     "vim --override-system-vi"
-    "zsh"
 )
 
 linuxify_install() {
     # Install all formulas
-    brew install "${formulas[@]}"
+    for (( i=0; i<${#formulas[@]}; i++ )); do
+        brew install "${formulas[i]}"
+    done
 
     # Change default shell to brew-installed /usr/local/bin/bash
     grep -qF '/usr/local/bin/bash' /etc/shells || echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
@@ -64,15 +65,9 @@ linuxify_install() {
     # One can either codesign the binary as per https://sourceware.org/gdb/wiki/BuildingOnDarwin
     # Or, on 10.12 Sierra or later with SIP, declare `set startup-with-shell off` in `~/.gdbinit`:
     grep -qF 'set startup-with-shell off' ~/.gdbinit || echo 'set startup-with-shell off' | sudo tee -a ~/.gdbinit
-
-    # Review installation to troubleshoot issues
-    brew doctor
 }
 
 linuxify_uninstall() {
-    # Uninstall all formulas
-    brew uninstall "${formulas[@]}"
-
     # Change default shell back to macOS /bin/bash
     sudo sed -i '/\/usr\/local\/bin\/bash/d' /etc/shells
     chsh -s /bin/bash
@@ -80,8 +75,10 @@ linuxify_uninstall() {
     # Remove gdb fix
     sed -i '/set startup-with-shell off/d' ~/.gdbinit
 
-    # Review installation to troubleshoot issues
-    brew doctor
+    # Uninstall all formulas in reverse order
+    for (( i=${#formulas[@]}-1; i>=0; i-- )); do
+        brew uninstall $(echo "${formulas[i]}" | cut -d ' ' -f1)
+    done
 }
 
 linuxify_info() {
@@ -104,6 +101,8 @@ main() {
         echo "Invalid usage"
         exit
     fi
+
+    brew doctor
 }
 
 main "$@"
